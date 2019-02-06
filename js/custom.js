4576ea7fba7a1c4321b05d2815608b785b30e153
stellakaniaru <stellakaniaru16@gmail.com>
2019-02-07 01:53:47 +0300

    Revert "consistent use of single quotes"
    This reverts commit 54859c7aa03d399cd7610562d5a70b79bc24b2da.

--- js/custom.js
+++ js/custom.js
@@ -85,7 +85,7 @@ $(document).ready(function(){
           var it = itemsPerSlide - (totalItems - idx);
           for (var i=0; i<it; i++) {
               // append slides to end
-              if (e.direction=='left') {
+              if (e.direction=="left") {
                   $('.carousel-item').eq(i).appendTo('.carousel-inner');
               }
               else {
